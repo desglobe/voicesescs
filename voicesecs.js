05e0a2eac72b80bb294f10a78da11ad70c4d1670
opeyemi akintunde <opeyemiakintunde10@gmail.com>
2021-06-23 21:06:57 +0100

    Phase 1 upload

---
+++ voicesecs.js
@@ -0,0 +1,7 @@
+var newDawn = document.getElementById("clearView");
+            function showMenu() {
+                clearView.style.right = "0px";
+            }
+            function hideMenu() {
+                clearView.style.right = "-300px";
+            }
