df35153df3088d369afef5a23247c14ddd90c939
opeyemi akintunde <opeyemiakintunde10@gmail.com>
2021-06-27 01:30:36 +0100

    Added responsiveness feature for Tablets, and Mobile devices sizes

--- voicesecs.js
+++ voicesecs.js
@@ -1,7 +1,7 @@
 var newDawn = document.getElementById("clearView");
             function showMenu() {
-                clearView.style.right = "0px";
+                newDawn.style.right = "0px";
             }
             function hideMenu() {
-                clearView.style.right = "-300px";
+                newDawn.style.right = "-300px";
             }
